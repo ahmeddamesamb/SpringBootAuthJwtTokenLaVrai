92e811e3157236c89977e5bd161c84abfeb02c01
Ahmed Dame Samb <asamb650@gmail.com>
2023-02-22 11:31:34 +0000

    authentification jwt token spring boot

--- src/main/java/com/example/authwithkeys/controller/AuthController.java
+++ src/main/java/com/example/authwithkeys/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 @RestController
 @RequestMapping("/api/auth")
